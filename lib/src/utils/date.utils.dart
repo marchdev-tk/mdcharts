2dc1e1c4e636dead60ad0e84169fb4916ed32979
OlehMarch <oleh.marchenko.95@gmail.com>
2022-03-19 01:12:29 +0200

    added copyright

--- lib/src/utils/date.utils.dart
+++ lib/src/utils/date.utils.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2022, the MarchDev Toolkit project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 extension DateTimeExtension on DateTime {
   DateTime get date => DateTime(year, month, day);
   DateTime get startOfMonth => DateTime(year, month);
