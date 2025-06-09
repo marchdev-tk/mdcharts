21dabbd1ad847466dcada36d3096432a8369d044
Oleh Marchenko <oleh.marchenko.95@gmail.com>
2025-06-09 16:32:38 +0200

    fixed drop line painter

--- lib/src/common/gridaxis/painter.dart
+++ lib/src/common/gridaxis/painter.dart
@@ -192,15 +192,25 @@ class GridAxisPainter extends CustomPainter {
     pathX.moveTo(0, point.dy);
     pathY.moveTo(point.dx, size.height);
 
-    final countX = (point.dx / (dashWidth + gapWidth)).round();
+    final countX = (point.dx / (dashWidth + gapWidth)).ceil();
     for (var i = 1; i <= countX; i++) {
+      if (i == countX) {
+        pathX.lineTo(point.dx, point.dy);
+        break;
+      }
+
       pathX.relativeLineTo(dashWidth, 0);
       pathX.relativeMoveTo(gapWidth, 0);
     }
 
     final countY =
-        ((size.height - point.dy) / (dashWidth + gapWidth)).round().abs();
+        ((size.height - point.dy) / (dashWidth + gapWidth)).ceil().abs();
     for (var i = 1; i <= countY; i++) {
+      if (i == countY) {
+        pathY.lineTo(point.dx, point.dy);
+        break;
+      }
+
       pathY.relativeLineTo(0, -dashWidth);
       pathY.relativeMoveTo(0, -gapWidth);
     }
